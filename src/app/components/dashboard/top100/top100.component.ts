4a66d55a24adbbcf7737a9c0f18e33ff66c0cf97
ivan-arroyo2 <i.arroyo.burguillo@gmail.com>
2024-02-21 11:46:31 +0100

    Initial Commit

---
+++ src/app/components/dashboard/top100/top100.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top100',
+  standalone: true,
+  imports: [],
+  templateUrl: './top100.component.html',
+  styleUrl: './top100.component.scss'
+})
+export class Top100Component {
+
+}
