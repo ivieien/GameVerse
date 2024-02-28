859543d0c51131de29b119939e115a01bdbdd0e1
Dani <daniiel122002@gmail.com>
2024-02-28 09:42:19 +0100

    funcion menu desplegable

--- src/app/components/layout/header/header/header.component.ts
+++ src/app/components/layout/header/header/header.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-links: string[] = ['Inicio','Juegos','Noticias','Videos','Reseñas'];
+  links: string[] = ['Inicio', 'Juegos', 'Noticias', 'Videos', 'Analisis y reseñas', 'Lo ultimo'];
+
+  menuAbierto: boolean = false;
+
+  MostrarMenuDesplegable() {
+    this.menuAbierto = !this.menuAbierto;
+  }
 }
