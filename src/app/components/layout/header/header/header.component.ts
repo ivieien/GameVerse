46d3320ac6c2cca18571cba5b650b7dc1bc7aa7d
ivan-arroyo2 <i.arroyo.burguillo@gmail.com>
2024-03-01 21:44:06 +0100

    feature - Muchas mierdas nuevas

--- src/app/components/layout/header/header/header.component.ts
+++ src/app/components/layout/header/header/header.component.ts
@@ -1,16 +1,30 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
+  animations: [
+    trigger('expandSearch', [
+      state('collapsed', style({ width: '0' })),
+      state('expanded', style({ width: '150px' })),
+      transition('collapsed => expanded', animate('300ms ease-out')),
+      transition('expanded => collapsed', animate('300ms ease-in'))
+    ])
+  ]
 })
 export class HeaderComponent {
   links: string[] = ['Inicio', 'Juegos', 'Noticias', 'Videos', 'Analisis y reseñas', 'Lo ultimo'];
+  searchExpanded: boolean = false;
 
   menuAbierto: boolean = false;
 
   MostrarMenuDesplegable() {
     this.menuAbierto = !this.menuAbierto;
+  }
+
+  toggleSearch() {
+    this.searchExpanded = !this.searchExpanded;
   }
 }
