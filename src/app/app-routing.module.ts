2771cbcafda65962bf5b6e1a0d14fb375da53a20
Dani <daniiel122002@gmail.com>
2024-03-22 12:58:41 +0100

    rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,18 +4,16 @@ import { JuegosComponent } from './components/juegos/juegos/juegos.component';
 import { NoticiasComponent } from './components/noticias/noticias/noticias.component';
 import { VideosComponent } from './components/videos/videos/videos.component';
 import { AnalisisComponent } from './components/analisis/analisis/analisis.component';
-import { LoginComponent } from './components/login/login/login.component';
 import { LoUltimoComponent } from './components/lo-ultimo/lo-ultimo/lo-ultimo.component';
 import { InicioComponent } from './components/inicio/inicio/inicio.component';
 
 const routes: Routes = [
-  { path: 'inicio', component: InicioComponent },
-  { path: 'juegos', component: JuegosComponent },
-  { path: 'noticias', component: NoticiasComponent },
+  { path: '', component: InicioComponent },
+  { path: 'games', component: JuegosComponent },
+  { path: 'news', component: NoticiasComponent },
   { path: 'videos', component: VideosComponent },
-  { path: 'analisis', component: AnalisisComponent },
-  { path: 'lo ultimo', component: LoUltimoComponent },
-  { path: 'login', component: LoginComponent },
+  { path: 'reviews', component: AnalisisComponent },
+  { path: 'the-latest', component: LoUltimoComponent },
 ];
 
 @NgModule({
